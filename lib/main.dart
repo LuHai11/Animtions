9b71bae1d86b4e972f0e4201a905b5f96d0f43cc
userlu <sea891217@yahoo.com>
2022-11-30 03:42:48 +0800

    animation-2

--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,65 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  static const String _title = 'airplanaAnimation';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      title: _title,
+      home: Scaffold(
+        appBar: AppBar(title: const Text(_title)),
+        body: const MyStatefulWidget(),
       ),
-      home: const MyHomePage(title: 'animation'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({super.key});
+
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  double startposition = 600.0;
-  double planeposition = 0.0;
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
   static const IconData flight = IconData(0xe297, fontFamily: 'MaterialIcons');
-  void _planeup(){
+  double padValue = -0.15;
+  void _updatePadding(double value) {
     setState(() {
-      planeposition-=20;
+      padValue = value;
     });
   }
+  void _downdatePadding() {
+    setState(() {
+      padValue = -0.15;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body:Center(
-        child: Column(
-        children: <Widget>[
-          AnimatedContainer(
-            padding: EdgeInsets.only(top:startposition-20),
-            child: Icon(flight),
-              height: 0-planeposition,
-              duration:const Duration(seconds: 2)
-          ),
-          Container(
-            padding: EdgeInsets.only(top:startposition),
-          child:
-          ElevatedButton.icon(
-              onPressed: (){_planeup();},
-              icon: Icon(flight),
-              label: Text("起飛")
-          )
-          ),
-        ],
-      ),
-    )
+    return Stack(
+      alignment: Alignment.center,
+      children: <Widget>[
+        AnimatedContainer(duration: Duration(seconds:3),
+          alignment:Alignment(0, padValue),
+          onEnd: _downdatePadding,
+          curve: Curves.easeOutQuart,
+          child:Container(child:Icon(flight)),
+        ),
+        ElevatedButton.icon(
+          onPressed: (){
+            _updatePadding(padValue == -0.15 ? -1.0 : -0.15);
+          },
+          icon:Icon(flight,color: Colors.white),
+          label: Text('起飛'),
+        ),
+      ],
     );
   }
 }
+
